62cdb3566f476acbe13f4b45e1c69f56496e83e3
Scheyk <kathrin.dostal@gmail.com>
2020-10-30 10:17:43 +0100

    basic structure

--- ST1CP4-KathrinSchey/Program.cs
+++ ST1CP4-KathrinSchey/Program.cs
@@ -6,7 +6,62 @@ namespace ST1CP4_KathrinSchey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi! Whats your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}! How are you today? (1- busy, but ok :) | 2- pretty good, thanks | 3- never been better :D )");
+            var how = Convert.ToInt32(Console.ReadLine());
+            
+            if (how == 1)
+            {
+                Console.WriteLine("Great. Woud you like to use our express-delivery service? (1- sounds good | 2- no hurry)");
+                TheMethods one = new TheMethods();
+                one.ExpressDelivery();
+            }
+
+            if (how == 2 || how == 3)
+            {
+                Console.WriteLine("Woud you like to order some food? (1- yes please! | 2- not yet, yust wanted to chat)");
+                TheMethods some = new TheMethods();
+                some.OrderFood();
+            }
+
+            Console.WriteLine("What type of pizza woud you like?");
+            TheMethods typePizza = new TheMethods();
+            typePizza.TypeOfPizza();
+
+            Console.WriteLine("What size? (small | big | super family big pizza)");
+            string sizePizza = Console.ReadLine();
+
+            Console.WriteLine("Woud you like to add some drinks to your order? (1- no, thanks | 2- yes please)");
+            var askDrink = Convert.ToInt32(Console.ReadLine());
+
+            if (askDrink == 1)
+            {
+                TheMethods one = new TheMethods();
+                one.DeliveryTime();
+            } else
+            {
+                TheMethods drink = new TheMethods();
+                drink.Drinks();
+                TheMethods delivery = new TheMethods();
+                delivery.DeliveryTime();
+            }
+
+            TheMethods payment = new TheMethods();
+            payment.PaymentSystem();
+
+            TheMethods personData = new TheMethods();
+            personData.PersonData();            
+
+            Console.WriteLine($"Hi {name}, we are glad to show you the details of your order:");
+            Console.WriteLine($"{typePizza}, {sizePizza}, {askDrink} , to be delivered at {personData}");
+            Console.WriteLine($"The method you coose is {payment}");
+            Console.WriteLine($"");
+           
+
+
+
+
         }
     }
 }
---
+++ ST1CP4-KathrinSchey/TheMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ST1CP4_KathrinSchey
+{
+    class TheMethods
+    {
+        public void ExpressDelivery()
+        {            
+            var express = Convert.ToInt32(Console.ReadLine());
+
+            if (express == 1)
+                TypeOfPizza();
+            else
+                OrderFood();
+        }
+
+        public void OrderFood()
+        {            
+            var order = Convert.ToInt32(Console.ReadLine());
+
+            if (order == 1)
+                TypeOfPizza();
+            else
+                Console.WriteLine("Thank you. Wish you a nice day.");
+        }
+
+        public void TypeOfPizza()
+        {
+            Console.WriteLine("magherita | quattro formaggi | tonno | diavolo | romana");
+            string typPizza = Console.ReadLine();
+        }
+
+        public void DeliveryTime()
+        {
+            Console.WriteLine("at what time shoud it be delivered?");
+            Console.WriteLine("1 hour | 2 hour | asap! i am very hungry...");
+            string timeDelivery = Console.ReadLine();
+        }
+
+        public void Drinks()
+        {
+            Console.WriteLine("juice | cola | beer");
+            string typeDrink = Console.ReadLine();
+        }
+
+        enum Payment
+        {
+             cash = 1,
+             creditCard,
+             PayPall,
+             Bitcoin
+        }
+
+        public void PaymentSystem() 
+        {
+            Console.WriteLine("What payment method do you prefere?");
+            Console.WriteLine("1- cash | 2- creditCard | 3- PayPall | 4- Bitcoin");
+            var payMethod = Convert.ToInt32(Console.ReadLine());
+
+            var theMethod = (Payment)payMethod;
+        }
+
+        public void PersonData()
+        {
+            Console.WriteLine("Now, we need your adress and phone number");
+            Console.WriteLine("At first, your street");
+            string street = Console.ReadLine();
+            Console.WriteLine("now the haus number");
+            var housNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your door number");
+            var doorNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please, enter your plz");
+            var plz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("and your place");
+            string place = Console.ReadLine();
+            Console.WriteLine("at last, your phone number please. (without special character)");
+            var phoneNumber = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}

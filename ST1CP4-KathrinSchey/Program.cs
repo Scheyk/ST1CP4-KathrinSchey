de149612fa122b8a16bd8aa646fc06608b4d1780
Scheyk <kathrin.dostal@gmail.com>
2020-10-30 08:11:22 +0100

    Projektdateien hinzufügen.

---
+++ ST1CP4-KathrinSchey/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ST1CP4_KathrinSchey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

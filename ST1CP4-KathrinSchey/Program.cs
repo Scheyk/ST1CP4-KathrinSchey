70dbfe4422ecf4192fc67f721145430a3c083cd1
Scheyk <kathrin.dostal@gmail.com>
2020-10-31 09:59:09 +0100

    okey...am richtigen weg!

--- ST1CP4-KathrinSchey/Program.cs
+++ ST1CP4-KathrinSchey/Program.cs
@@ -1,67 +1,211 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net.Http.Headers;
 
 namespace ST1CP4_KathrinSchey
 {
     class Program
     {
-        static void Main(string[] args)
+        // ################################################# global var
+        public static string pay;
+        public static string chooseDrink;
+        public static string typPizza;
+        public static string street;
+        public static int housNumber;
+        public static int doorNumber;
+        public static int plz;
+        public static string place;
+        public static int phoneNumber;
+        public static string name;
+        
+
+        enum SizePizza
         {
-            Console.WriteLine("Hi! Whats your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name}! How are you today? (1- busy, but ok :) | 2- pretty good, thanks | 3- never been better :D )");
-            var how = Convert.ToInt32(Console.ReadLine());
+            small = 1,
+            big,
+            family
+        }
+        
+        // ################################################# methods start
+
+        public static void PaymentSystem() // <---- for the pay methods. use on line 212
+        {
             
-            if (how == 1)
+            Console.WriteLine("What payment method do you prefere?");
+            Console.WriteLine("1- cash | 2- creditCard | 3- PayPall | 4- Bitcoin");
+            
+            pay = Console.ReadLine();  // <---- pay = global variable
+
+            if (pay == "1")
+                pay = "cash";
+            else if (pay == "2")
+                pay = "credit card";
+            else if (pay == "3")
+                pay = "PayPall";
+            else if (pay == "4")
+                pay = "BitCoin";
+            else
+            {
+                pay = "cash";
+            } 
+
+        }
+        
+        
+
+        public static void WhatTime()  // <---- use on line 205 
+        {            
+            Console.WriteLine("at what time shoud it be delivered?");
+            Console.WriteLine("1 hour | 2 hour | asap! i am very hungry... (just enter 1,2 or 3)");
+            string timeDelivery = Console.ReadLine();            
+        }
+
+        public static void TypeOfPizza()  // <---- use on lines 157, 167, 185
+        {
+
+            Console.WriteLine("1- magherita | 2- quattro formaggi | 3- tonno | 4- diavolo | 5- romana");
+            typPizza = Console.ReadLine();
+
+            switch (typPizza)
+            {
+                case "1":
+                    Console.WriteLine($"You choosed Magherita good choice {name}");
+                    typPizza = "Margherita";
+                    break;
+                case "2":
+                    Console.WriteLine("quattro formaggi");
+                    break;
+                case "3":
+                    Console.WriteLine("tonno");
+                    break;
+                case "4":
+                    Console.WriteLine("diavolo");
+                    break;
+                case "5":
+                    Console.WriteLine("romana");
+                    break;
+                default:
+                    Console.WriteLine("nothing choose");
+                    break;
+            }
+        }
+
+        public static void PersonData() // <--- use on line 213
+        {
+            Console.WriteLine("Now, we need your adress and phone number");
+            Console.WriteLine("At first, your street");
+            street = Console.ReadLine();
+            Console.WriteLine("now the haus number");
+            housNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your door number");
+            doorNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please, enter your plz");
+            plz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("and your place");
+            place = Console.ReadLine();
+            Console.WriteLine("at last, your phone number please. (without special character)");
+            phoneNumber = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        // +++++++++++++++++++++++++++++++++++++++ main start
+        static void Main(string[] args)
+        { 
+            // hello part :) 
+            Console.WriteLine("Hi! Whats your name?");
+            //Console.WriteLine("to serius?- press 1");
+            //var serius = Convert.ToInt32(Console.ReadLine());
+            /*
+            if(serius == 1)
+            {
+                Console.WriteLine("Ok. Wish you a nice day!");
+                System.Environment.Exit(0);
+            }*/
+
+            //Console.WriteLine("Ok. Not to sirius. fine :) ! Whats your name?");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}! Welcome in our Casa Roma Trattoria! \n How are you today? (1- busy, but ok :) | 2- pretty good, thanks | 3- never been better :D )");
+            var how = Convert.ToInt32(Console.ReadLine());
+
+            if (how == 1) // in Eile
+
             {
                 Console.WriteLine("Great. Woud you like to use our express-delivery service? (1- sounds good | 2- no hurry)");
-                TheMethods one = new TheMethods();
-                one.ExpressDelivery();
-            }
+                var express = Convert.ToInt32(Console.ReadLine());
+                if (express == 1) // express Liefern JA
+                {
+                    Console.WriteLine("What type of pizza woud you like?");
+                    TypeOfPizza();
 
-            if (how == 2 || how == 3)
+                } else  //  Express KLiefern Nein
+                {
+                    Console.WriteLine("Woud you like to order some food? (1- yes please! | 2- not yet, yust wanted to chat)");
+                    var orderFood = Convert.ToInt32(Console.ReadLine());
+
+                    if (orderFood == 1) // Will Bestellen aber nicht express
+                    {
+                        Console.WriteLine("What type of pizza woud you like?");
+                        TypeOfPizza();
+                    }
+                    else // Will nicht bestellen exit Chat Beenden Programm
+                    {
+                        Console.WriteLine("ok. wish you a nice day");
+                        System.Environment.Exit(0);
+
+                    }
+                } // Kein Express Ende
+
+            } else // user ist Chill und hat keine Eile Noch nicht nach bestellung gefragt
             {
                 Console.WriteLine("Woud you like to order some food? (1- yes please! | 2- not yet, yust wanted to chat)");
-                TheMethods some = new TheMethods();
-                some.OrderFood();
+                var orderFood = Convert.ToInt32(Console.ReadLine());
+
+                if (orderFood == 1)
+                {
+                    Console.WriteLine("What type of pizza woud you like?");
+                    TypeOfPizza();
+                }
+                else
+                {
+                    Console.WriteLine("ok. wish you a nice day");
+                    System.Environment.Exit(0);
+                }
+
             }
 
-            Console.WriteLine("What type of pizza woud you like?");
-            TheMethods typePizza = new TheMethods();
-            typePizza.TypeOfPizza();
-
-            Console.WriteLine("What size? (small | big | super family big pizza)");
-            string sizePizza = Console.ReadLine();
+            Console.WriteLine("What size?");
+            Console.WriteLine("1- small | 2- big | 3- family");
+            var pizzaSize = Convert.ToInt32(Console.ReadLine());
+            var printSize = (SizePizza)pizzaSize;
 
             Console.WriteLine("Woud you like to add some drinks to your order? (1- no, thanks | 2- yes please)");
             var askDrink = Convert.ToInt32(Console.ReadLine());
 
             if (askDrink == 1)
             {
-                TheMethods one = new TheMethods();
-                one.DeliveryTime();
+                WhatTime();
+                
             } else
             {
-                TheMethods drink = new TheMethods();
-                drink.Drinks();
-                TheMethods delivery = new TheMethods();
-                delivery.DeliveryTime();
+                TheMethods objChoosenDrinks = new TheMethods();
+                
+               chooseDrink = objChoosenDrinks.ChooseDrinks("0"); 
             }
 
-            TheMethods payment = new TheMethods();
-            payment.PaymentSystem();
-
-            TheMethods personData = new TheMethods();
-            personData.PersonData();            
+            PaymentSystem();
+            PersonData();
 
             Console.WriteLine($"Hi {name}, we are glad to show you the details of your order:");
-            Console.WriteLine($"{typePizza}, {sizePizza}, {askDrink} , to be delivered at {personData}");
-            Console.WriteLine($"The method you coose is {payment}");
-            Console.WriteLine($"");
-           
-
-
-
+            Console.WriteLine($"{typPizza}, {printSize}, {chooseDrink} to be delivered at");
+            Console.WriteLine($"The method you coose is {pay}");
+            Console.WriteLine($"street: {street} housenumber: {housNumber}");
+            Console.WriteLine($"plz: {plz} city: {place}");
+            Console.WriteLine($"your phonenumber is: {phoneNumber}");
 
         }
     }
 }
+
--- ST1CP4-KathrinSchey/TheMethods.cs
+++ ST1CP4-KathrinSchey/TheMethods.cs
@@ -1,21 +1,26 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 using System.Text;
 
 namespace ST1CP4_KathrinSchey
 {
     class TheMethods
     {
+        /*
         public void ExpressDelivery()
-        {            
+        {
+            
             var express = Convert.ToInt32(Console.ReadLine());
 
             if (express == 1)
                 TypeOfPizza();
             else
                 OrderFood();
-        }
+        }*/
 
+        /*
         public void OrderFood()
         {            
             var order = Convert.ToInt32(Console.ReadLine());
@@ -24,59 +29,71 @@ namespace ST1CP4_KathrinSchey
                 TypeOfPizza();
             else
                 Console.WriteLine("Thank you. Wish you a nice day.");
-        }
+        }*/
 
-        public void TypeOfPizza()
-        {
-            Console.WriteLine("magherita | quattro formaggi | tonno | diavolo | romana");
-            string typPizza = Console.ReadLine();
-        }
 
-        public void DeliveryTime()
+        /*
+
+        Console.WriteLine("1- magherita | 2- quattro formaggi | 3- tonno | 4- diavolo | 5- romana");
+                    var typPizza = Convert.ToInt32(Console.ReadLine());
+        var printPizzaTyp = (TypPizza)typPizza;
+        */
+
+        /*
+    public void DeliveryTime()
+    {
+        Console.WriteLine("at what time shoud it be delivered?");
+        Console.WriteLine("1 hour | 2 hour | asap! i am very hungry...");
+        string timeDelivery = Console.ReadLine();
+    }*/
+
+        /*
+        public string Drinks(string drink)
         {
-            Console.WriteLine("at what time shoud it be delivered?");
-            Console.WriteLine("1 hour | 2 hour | asap! i am very hungry...");
-            string timeDelivery = Console.ReadLine();
+            Console.WriteLine("What drink you whana choose?");
+            Console.WriteLine("juice | cola | beer");
+            return drink;
+        }*/
+
+        public string ChooseDrinks(string choosenDrink)  // <----- for the drinks. use on line 209
+        {
+            Console.WriteLine("What drink you whana choose?");
+
+           
+            Drinks();
+
+            choosenDrink = Console.ReadLine(); // <---- chooseDrink = global variable 
+
+            if (choosenDrink == "1")
+                choosenDrink = "juce";
+            else if (choosenDrink == "2")
+                choosenDrink = "cola";
+            else if (choosenDrink == "3")
+                choosenDrink = "beer";
+            else
+            {
+                choosenDrink = "nothing to drink";
+            }
+            return choosenDrink;
         }
 
         public void Drinks()
         {
-            Console.WriteLine("juice | cola | beer");
-            string typeDrink = Console.ReadLine();
+            ArrayList drinks = new ArrayList(new string[] {"1- juice", "2- cola", "3- beer"});
+
+            for (int i = 0; i < drinks.Count; i++)
+            {
+                Console.Write(drinks[i] + " ");
+            }
+           
         }
 
-        enum Payment
-        {
-             cash = 1,
-             creditCard,
-             PayPall,
-             Bitcoin
-        }
 
-        public void PaymentSystem() 
-        {
-            Console.WriteLine("What payment method do you prefere?");
-            Console.WriteLine("1- cash | 2- creditCard | 3- PayPall | 4- Bitcoin");
-            var payMethod = Convert.ToInt32(Console.ReadLine());
 
-            var theMethod = (Payment)payMethod;
-        }
 
-        public void PersonData()
-        {
-            Console.WriteLine("Now, we need your adress and phone number");
-            Console.WriteLine("At first, your street");
-            string street = Console.ReadLine();
-            Console.WriteLine("now the haus number");
-            var housNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("your door number");
-            var doorNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please, enter your plz");
-            var plz = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("and your place");
-            string place = Console.ReadLine();
-            Console.WriteLine("at last, your phone number please. (without special character)");
-            var phoneNumber = Convert.ToInt32(Console.ReadLine());
-        }
+        
+        
+
+        
     }
 }
